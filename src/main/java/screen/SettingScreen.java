2cdd0429926d62bfbb876c43331a4e17ef1a594a
Chanhyuk Park <pgg6713@gmail.com>
2021-11-13 14:38:21 +0900

    [test][SETB-19] integration test

--- src/main/java/screen/SettingScreen.java
+++ src/main/java/screen/SettingScreen.java
@@ -1,6 +1,6 @@
 package screen;
 
 public class SettingScreen {
-    private static final int selectedScreenSize = 100;
-    private static final int selectedEnemyBulletInterval = 100;
+    private static final int selectedScreenSize = 50;
+    private static final int selectedEnemyBulletInterval = 50;
 }
