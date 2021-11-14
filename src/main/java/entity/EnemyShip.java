4df6cc411a0ac7acde631cbacdb55a35b7067807
kimsehw <seunghwan9703@gmail.com>
2021-11-15 00:50:29 +0900

    [add][SETB-45]bossExplosion추가

--- src/main/java/entity/EnemyShip.java
+++ src/main/java/entity/EnemyShip.java
@@ -187,7 +187,20 @@ public class EnemyShip extends Entity {
 	 */
 	public final void destroy() {
 		this.isDestroyed = true;
-		this.spriteType = SpriteType.Explosion;
+		switch(this.spriteType){
+			case BossA1:
+			case BossA2:
+			case BossB2:
+			case BossB1:
+			case BossC1:
+			case BossC2:
+				this.spriteType = SpriteType.BossExplosion;
+				break;
+			default:
+				this.spriteType = SpriteType.Explosion;
+				break;
+		}
+
 	}
 
 	/**
