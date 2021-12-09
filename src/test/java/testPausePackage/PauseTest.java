7e888a796f2f332631dfe98f63ce8fe9a42506fe
Nahyun Kim <62279962+nahyun27@users.noreply.github.com>
2021-12-09 23:33:50 +0900

    Setb 28/subfeat auto enter when5s no control (#44)
    * [add][SETB-28] 10s 키보드 입력 없으면 pause모드
    
    * add pauseTest
    
    Co-authored-by: kimdaye77 <kimdaye77@naver.com>

--- src/test/java/testPausePackage/PauseTest.java
+++ src/test/java/testPausePackage/PauseTest.java
@@ -1,13 +1,15 @@
 package testPausePackage;
+import engine.GameSettings;
+import engine.GameState;
 import engine.InputManager;
-import engine.Sound;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import screen.Screen;
-import screen.SettingScreen;
-import screen.TitleScreen;
+import screen.GameScreen;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static engine.Core.getInputManager;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -22,26 +24,42 @@ public class PauseTest {
         System.out.println("before");
     }
 
-    @DisplayName("키보드 입력시마다 starttime 갱신 잘 되는지 테스트")
+    @DisplayName("키보드 입력시마다 Input managing 잘 되는지 테스트")
     @Test
-    public void testReturnCoed() throws InterruptedException {
+    public void testInputManager() throws InterruptedException {
         InputManager inputManager = getInputManager();
 
-        //assertFalse(inputManager.isKeyDown(), "false");
+        assertFalse(inputManager.isKeyDown(1), "false");
+        assertFalse(inputManager.isActive(), "false");
+        inputManager.isPressed = true;
+        assertTrue(inputManager.isActive(), "false");
+
+        assertFalse(inputManager.isKeyDown(KeyEvent.VK_RIGHT), "false");
     }
 
-    @DisplayName("pause모드시 계속 pause모드 작동하고 r누를 때에만 풀리는지 테스트")
+    @DisplayName("게임스크린에서 update() 잘 되는지 테스트")
     @Test
-    public void testColors() {
+    public void testPause() throws NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        boolean bonusLife = true;
 
+        GameState gameState = new GameState(1, 0, 3, 0, 0);
 
-    }
+        GameSettings SETTINGS_LEVEL_1 =
+                new GameSettings(5, 4, 60, 2000, 1);
 
-    @DisplayName("pause모드시 계속 pause모드 작동하고 r누를 때에만 풀리는지 테스트")
-    @Test
-    public void testSpaceBar() {
+        GameScreen gameClass = new GameScreen(gameState,
+                SETTINGS_LEVEL_1,
+                bonusLife, 448, 520, 60);
 
+        Method method = gameClass.getClass().getDeclaredMethod("update");
+        method.setAccessible(true);
+        method.invoke(gameClass);
 
+        InputManager inputManager = getInputManager();
+        inputManager.isPressed = true;
+        assertTrue(inputManager.isActive(), "false");
+
+        assertFalse(inputManager.isKeyDown(KeyEvent.VK_RIGHT), "false");
     }
 
     @After
